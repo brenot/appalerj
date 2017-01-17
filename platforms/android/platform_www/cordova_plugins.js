6b68fc4fbc2cc3fdc6e21d605bf2b128c4b23d3a
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-01-17 09:50:39 -0200

    First commit v.1.4

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,45 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+        "id": "cordova-plugin-x-socialsharing.SocialSharing",
+        "pluginId": "cordova-plugin-x-socialsharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.1.0",
+    "cordova-plugin-splashscreen": "3.0.0",
+    "cordova-plugin-x-socialsharing": "5.0.7",
+    "cordova-plugin-wkwebview-engine": "1.0.0",
+    "cordova-plugin-inappbrowser": "1.3.0"
+}
+// BOTTOM OF METADATA
+});
