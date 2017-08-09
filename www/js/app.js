7aabcd41bbde47ff5880779699197eb236c044bc
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-08-09 15:12:00 -0300

    Update app

--- www/js/app.js
+++ www/js/app.js
@@ -1,31 +1,33 @@
 
 var app = {
 
-	contextPath: "http://apiportal.alerj.rj.gov.br"  
-	,contextPathPortal: "http://apialerj.rj.gov.br"  
+    contextPath: "http://apiportal.alerj.rj.gov.br",
+    //contextPathPortal: "http://apialerj.rj.gov.br", // --- direto no proderj
+    contextPathPortal: "http://apialerj.rj.gov.br",   // --- projetos especiais - alerj
+
+    contextPathPortal: "http://apiportal.alerj.rj.gov.br/api/v1.0/proderj",
+
+    //,contextPathPortal: "http://10.11.63.11:8040"
+	//,contextPathPortal: "http://10.11.63.11:8040"
 		
-	//,contextPathPortal: "http://10.11.63.11:8040"  
-		
-		
+    appPath: "/alerj",
 	
-	,appPath: "/alerj"
-	
-	,appName: ""
-    ,version: "1.5beta (Projetos Especiais)"
-	
-		
-	,crossdomain : false
-	,crossdomainPortal : false
+    appName: "",
+
+    version: "1.5beta (Projetos Especiais)",
+
+    crossdomain : false,
+    crossdomainPortal : false,
 	
 	//path a ser configurado quando os acessos serao crossdomain. Servidor de servicos que so aceita requisicoes AJAX GET.
-	,remoteServicePath : "/api/v1.0"
-	,remoteServicePathPortal : "/portal.alerj.service/api"
+    remoteServicePath : "/api/v1.0",
+    remoteServicePathPortal : "/portal.alerj.service/api",
 	
 	//path a ser configurado quando os acessos nao serao crossdomain.	
-	,servicePath : "/api/v1.0"
-	,servicePathPortal : "/api"
+    servicePath : "/api/v1.0",
+    servicePathPortal : "/api",
 	
-	,writeScript: function( path ){
+    writeScript: function( path ){
 		var script = [
 			"<","script"
 			," type=\"text/javascript\""
@@ -35,9 +37,9 @@ var app = {
 			,"</","script",">"
 		];
 		document.write( script.join( "" ) );
-	}
+	},
 
-	,writeCode: function( code ){
+	writeCode: function( code ){
 		var script = [
 			"<","script"
 			," type=\"text/javascript\""
@@ -47,9 +49,9 @@ var app = {
 			,"</","script",">"
 		];
 		document.write( script.join( "" ) );
-	}
+	},
 	
-	,writeLink: function( path ){
+	writeLink: function( path ){
 		var link = [
 			"<","link"
 			," rel=\"stylesheet\""
@@ -58,7 +60,7 @@ var app = {
 			,"</","link", ">"
 		];
 		document.write( link.join( "" ) );
-	}
+	},
 }
 if ( typeof console == "undefined" ) {
 	// Evita erro de console do firefox nao inicialisado.
